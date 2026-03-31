7e2db491d26eaae3a3e01978e8726d29806f36f3
Lisa-George-SodlanTech <vk1625350@gmail.com>
2026-03-31 12:45:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def health():
     return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
